8c02b5ebd9642c6d8f1923b1c9ffd5dcdcd10667
Peace Amichoh <121777763+Peacefri@users.noreply.github.com>
2026-03-30 14:34:42 -0500

    Add phone_number column to student table

--- ThinkNInk/thinknink_tables.sql
+++ ThinkNInk/thinknink_tables.sql
@@ -12,6 +12,7 @@ CREATE TABLE Students (
     first_name   VARCHAR(50)  NOT NULL,
     last_name    VARCHAR(50)  NOT NULL,
     email        VARCHAR(100) NOT NULL UNIQUE,
+    phone_number VARCHAR(20),
     password     VARCHAR(32)  NOT NULL
 );
 
